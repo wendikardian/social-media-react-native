2fad5639f541de694f031f71badd2782d24e7bd8
wendikardian <wendikardian@gmail.com>
2022-07-20 14:54:42 +0700

    -

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,12 +1,55 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, Image, TouchableOpacity, ScrollView } from 'react-native'
 import React from 'react'
+import {Button} from '../components/ButtonComponent.js'
+import {Input} from '../components/InputComponent.js'
 
 const LoginScreen = () => {
   return (
-    <View>
-      <Text>LoginScreen</Text>
-    </View>
+    <ScrollView contentContainerStyle={styles.scroll}>
+        <View style={styles.mainContainer}>
+            <View style={styles.imageContainer}>
+                <Image style={styles.image} source={require('../../assets/images/login.png')} />
+            </View>
+            <View style={styles.inputContainer}>
+                <Input title="Username" placeholder="Username" />
+            </View>
+            <Button text="Login"/>
+            <View style={styles.textContainer}>
+                <Text style={styles.text}>Don't have an account ? </Text>
+                <TouchableOpacity><Text style={styles.registerText}>Register</Text></TouchableOpacity>
+            </View>
+        </View>
+    </ScrollView>
   )
 }
+
+const styles = StyleSheet.create({
+    scroll : {
+        flexGrow : 1,
+    }, mainContainer : {
+        backgroundColor: '#E6E6FA',
+        flex: 1,
+        alignItems : 'center',
+        justifyContent : 'center',
+    }, imageContainer : {
+        marginTop: 32,
+        marginBottom: 15
+    }, image : {
+        width: 180,
+        height: 180
+    }, inputContainer : {
+        padding: 16,
+        width: '100%'
+    }, textContainer  : {
+        flexDirection: 'row',
+        marginTop: 16
+    }, text : {
+        fontSize: 16
+    }, registerText : {
+        color: '#1A5B0A',
+        fontSize: 16
+    }
+})
+
 
 export default LoginScreen
